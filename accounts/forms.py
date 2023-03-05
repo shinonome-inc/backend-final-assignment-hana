7b9f3508aac5823181146b96c71dd406d41a6b59
SShizawa <note87green@icloud.com>
2023-03-05 18:15:26 +0900

    2次レビュの修生

--- accounts/forms.py
+++ accounts/forms.py
@@ -14,5 +14,5 @@ class LoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
-            field.widget.attrs["class"] = "form-control"
+            # field.widget.attrs["class"] = "form-control"
             field.widget.attrs["placeholder"] = field.label
